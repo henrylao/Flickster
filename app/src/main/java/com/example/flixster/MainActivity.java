51a845cff5df79f8ac5143d5428808e50d54356f
Henry <hclao1995@gmail.com>
2021-01-23 16:59:49 -0800

    Add logging of Movies from JSON

--- app/src/main/java/com/example/flixster/MainActivity.java
+++ app/src/main/java/com/example/flixster/MainActivity.java
@@ -39,6 +39,8 @@ public class MainActivity extends AppCompatActivity {
                     JSONArray results = jsonObject.getJSONArray("results");
                     Log.i(TAG, "Results: " + results);
                     movies = Movie.fromJsonArray(results);
+                    Log.i(TAG, "Movies: " + movies.size());
+
                 } catch (JSONException e) {
                     Log.e(TAG, "Hit JSON exxception", e);
                     e.printStackTrace();
