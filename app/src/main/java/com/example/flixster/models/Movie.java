1b0d84e025f1c2702bcadeaebb8375dca2139b2f
Henry <hclao1995@gmail.com>
2021-01-23 16:38:42 -0800

    Create Movie.java

---
+++ app/src/main/java/com/example/flixster/models/Movie.java
@@ -0,0 +1,28 @@
+package com.example.flixster.models;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Movie {
+    String posterPath;
+    String title;
+    String overview;
+
+    public Movie(JSONObject jsonObject) throws JSONException {
+        posterPath=jsonObject.getString("poster_path");
+        title = jsonObject.getString("title");
+        overview =jsonObject.getString("overview");
+    }
+
+    public static List<Movie> fromJsonArray(JSONArray movieJsonArray){
+        List<Movie> movies = new ArrayList<>();
+        for (int i =0; i < movieJsonArray.length(); i++){
+            movies.add(new Movie(movieJsonArray.getJSONObject(i)));
+        }
+        return movies;
+    }
+}
