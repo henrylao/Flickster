4f1aa0fc3ed020fbcb2b8509d40e21c4e8be5b1b
Henry <hclao1995@gmail.com>
2021-02-05 07:35:49 -0800

    Add on top routing to DetailActivity

--- app/src/main/java/com/example/flixster/adapters/MovieAdapter.java
+++ app/src/main/java/com/example/flixster/adapters/MovieAdapter.java
@@ -1,6 +1,7 @@
 package com.example.flixster.adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -8,16 +9,20 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.RatingBar;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.example.flixster.DetailActivity;
 import com.example.flixster.R;
 import com.example.flixster.models.Movie;
 
 import org.jetbrains.annotations.NotNull;
+import org.parceler.Parcels;
 
 import java.util.List;
 
@@ -59,6 +64,7 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
 
     // Representation of a row in the Recycler View
     public class ViewHolder extends RecyclerView.ViewHolder {
+        RelativeLayout container;
         TextView tvTitle;
         TextView tvOverview;
         ImageView ivPoster;
@@ -72,12 +78,30 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
             tvOverview = itemView.findViewById(R.id.tvOverview);
             ivPoster = itemView.findViewById(R.id.ivPoster);
             rbStarRating = itemView.findViewById(R.id.rbStarRating);
+            container = itemView.findViewById(R.id.container);
 //            tvRating = itemView.findViewById(R.id.tvRating);
         }
 
         public void bind(@NotNull Movie movie) {
             tvTitle.setText(movie.getTitle());
             tvOverview.setText(movie.getOverview());
+
+            // 1. Register click listener on the whole row-container
+            container.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // 2. Action to be taken is a navigation to a new activity on tap
+                    Intent i = new Intent(context, DetailActivity.class);
+                    i.putExtra("title", movie.getTitle());
+                    i.putExtra("overview", movie.getOverview());
+                    i.putExtra("vote_average", movie.getVoteAverage());
+                    i.putExtra("movie", Parcels.wrap(movie));
+                    context.startActivity(i);
+
+//                    Toast.makeText(context, movie.getTitle(), Toast.LENGTH_SHORT).show();   // DEBUG
+
+                }
+            });
 //            tvRating.setText(Double.toString(movie.getVoteAverage()));
 //            movieId = movie.getMovieId();
 //            rbStarRating.setNumStars(5);
