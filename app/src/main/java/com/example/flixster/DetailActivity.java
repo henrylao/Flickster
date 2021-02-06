109038db7cb46941c8dfdcff0122caa42aa11b85
Henry <hclao1995@gmail.com>
2021-02-06 05:59:28 -0800

    Cut API key to be stored in XML

--- app/src/main/java/com/example/flixster/DetailActivity.java
+++ app/src/main/java/com/example/flixster/DetailActivity.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.RatingBar;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.codepath.asynchttpclient.AsyncHttpClient;
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
@@ -21,7 +20,7 @@ import org.parceler.Parcels;
 import okhttp3.Headers;
 
 public class DetailActivity extends YouTubeBaseActivity {
-    public static final String YOUTUBE_API_KEY = "AIzaSyA4MED-Xlb5wc03V10Z8Icfte8y4Zwn5-8";
+    public String YOUTUBE_API_KEY;
     public static final String VIDEOS_URL = "https://api.themoviedb.org/3/movie/%d/videos?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
 
     TextView tvTitle;
@@ -31,6 +30,8 @@ public class DetailActivity extends YouTubeBaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        YOUTUBE_API_KEY = this.getString(R.string.YOUTUBE_API_KEY);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
@@ -76,13 +77,11 @@ public class DetailActivity extends YouTubeBaseActivity {
             public void onInitializationSuccess(YouTubePlayer.Provider provider,
                                                 YouTubePlayer youTubePlayer, boolean b) {
                 // do any work here to cue video, play video, etc.
-//                youTubePlayer.cueVideo(Integer.toString(movie.getid()));
                 // autostart trailer for popular movies
                 if (movie.getVoteAverage() >= 5) {
 //                    Toast.makeText(DetailActivity.this, "Autoplay", Toast.LENGTH_SHORT).show();   // DEBUG
                     youTubePlayer.loadVideo(youtubeKey);
-                }
-                else
+                } else
                     youTubePlayer.cueVideo(youtubeKey);
             }
 
