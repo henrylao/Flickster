b18d539d4eb3c3e02617bb046545be16ed05bb9e
Henry <hclao1995@gmail.com>
2021-02-07 08:34:33 -0800

    Cut stars on landscape/portrait

--- app/src/main/java/MovieDBConstants.java
+++
@@ -1,6 +0,0 @@
-public interface MovieDBConstants {
-    // Constants
-    // API base URL
-    public final static String API_BASE_URL = "https://api.themoviedb.org/3";
-    // insert all the other REST api extensions
-}
